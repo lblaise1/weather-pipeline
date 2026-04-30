bf47f0be9d0690b169e2d68d178e5426cb7af7f8
LB <loubens.blaise@gmail.com>
2026-04-30 15:43:57 -0400

    Add retries and pass Postgres env vars to dbt container

--- airflow/dags/orchestrator.py
+++ airflow/dags/orchestrator.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
@@ -10,10 +11,22 @@ from docker.types import Mount
 sys.path.append('/opt/airflow/api-request')
 from insert_records import main
 
+
+
+# Near the top of the file
+PG_USER = os.environ['POSTGRES_USER']
+PG_PASSWORD = os.environ['POSTGRES_PASSWORD']
+PG_DB = os.environ['POSTGRES_DB']
+
+
 default_args = {
     'description': 'A DAG to orchestrate data',
     'start_date': datetime(2024, 4, 30),
     'catchup': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
+    'retry_exponential_backoff': True,
+    'max_retry_delay': timedelta(minutes=30),
 }
 
 dag = DAG(
@@ -30,9 +43,14 @@ with dag:
 
     task2 = DockerOperator(
         task_id ='transform_data_task',
-        image= 'ghcr.io/dbt-labs/dbt-postgres:1.9.latest',
+        image= 'ghcr.io/dbt-labs/dbt-postgres:1.9.0',
         command = 'run',
         working_dir='/usr/app',
+        environment={
+            'POSTGRES_USER': PG_USER,
+            'POSTGRES_PASSWORD': PG_PASSWORD,
+            'POSTGRES_DB': PG_DB,
+        },
         mounts=[
             Mount(source='/home/loubens/repos/weather/dbt/my_project',
                   target='/usr/app', type='bind'),
